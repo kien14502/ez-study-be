8a7a45d8587ff3247f61fb567c27906818ef0bde
Vũ Đại An <90129616+vudaian24@users.noreply.github.com>
2025-11-24 22:28:41 +0700

    Fix ci cd docker (#39)
    * test: cd docker 2
    
    * test: ci cd docker
    
    * test: cd docker 5
    
    * tesssttt

--- src/common/validation-schema.ts
+++ src/common/validation-schema.ts
@@ -1,13 +1,13 @@
 import Joi from 'joi';
 
 import ConfigKey from './config-key';
-import { DEFAULT_PORT, NodeEnv } from './constants';
+import { NodeEnv } from './constants';
 
 export default Joi.object({
   [ConfigKey.NODE_ENV]: Joi.string().valid(...Object.values(NodeEnv)),
-  [ConfigKey.PORT]: Joi.number().default(DEFAULT_PORT),
+  // [ConfigKey.PORT]: Joi.number().default(DEFAULT_PORT),
 
-  [ConfigKey.MONGO_DATABASE_CONNECTION_STRING]: Joi.string().required(),
+  // [ConfigKey.MONGO_DATABASE_CONNECTION_STRING]: Joi.string().required(),
 
   [ConfigKey.SWAGGER_ENABLED]: Joi.boolean().default(false),
 });
