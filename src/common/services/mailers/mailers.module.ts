c6b023527dbe6cee8a1896e4387b3e0d87ff3bc7
vudaian24 <vudaian12a7@gmail.com>
2025-11-30 22:49:52 +0700

    fix mail and ci

--- src/common/services/mailers/mailers.module.ts
+++ src/common/services/mailers/mailers.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { Module } from '@nestjs/common';
 
 import { EmailConsumerController } from './mailer.consumer';
 import { MailersService } from './mailers.service';
